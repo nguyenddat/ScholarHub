15c59a67f3dcd688f0e4a5e36681827386d57f2c
Đạt Đình <datnd.b23cc032@stu.ptit.edu.vn>
2025-05-08 08:50:56 +0700

    fix docker

--- backend/models/Profile.py
+++ backend/models/Profile.py
@@ -7,11 +7,7 @@ from sqlalchemy.dialects.postgresql import UUID
 from helpers.Enums import *
 from models.BaseClass import BareBaseModel, Base
 
-<<<<<<< HEAD
 class Profile(Base):
-=======
-class Profile(BareBaseModel):
->>>>>>> origin/dev_foggo_ben_bo_ho
     __tablename__ = 'profiles'
 
     user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), primary_key=True)
@@ -27,9 +23,4 @@ class Profile(BareBaseModel):
     self_introduction = Column(Text)
     is_public = Column(Boolean, default=False)
 
-<<<<<<< HEAD
     user = relationship("User", back_populates="profile")
-    preferences = relationship("Preference", back_populates="profile")
-=======
-    user = relationship("User", back_populates="profile")
->>>>>>> origin/dev_foggo_ben_bo_ho
