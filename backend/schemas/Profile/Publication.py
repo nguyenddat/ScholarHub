b9e89213ff65077125cf232b8b35e7ae91285f60
namn44241 <nguyenvandoaaaaa@gmail.com>
2025-06-11 01:42:54 +0700

    [ADD] api publications  profile - cần update bảng publications

--- backend/schemas/Profile/Publication.py
+++ backend/schemas/Profile/Publication.py
@@ -9,13 +9,13 @@ class PublicationCreateRequest(BaseModel):
     title: str
     type: str = Field(default='journal')  # Loại publication, mặc định là 'journal'
     venue_name: str  # Tên nơi công bố (ví dụ: tạp chí, hội nghị...)
-    publish_date: date  # Ngày công bố
+    publication_date: Optional[date] = None
     url: Optional[str] = None  # URL của publication, nếu có
 
 class PublicationUpdateRequest(BaseModel):
     id: str  # ID của publication cần cập nhật
     title: Optional[str] = None  # Tiêu đề có thể cập nhật
-    type: Optional[str] = Field(default='journal')  # Loại publication, mặc định là 'journal'
+    type: Optional[str] = None
     venue_name: Optional[str] = None  # Tên nơi công bố có thể cập nhật
-    publish_date: Optional[date] = None  # Ngày công bố có thể cập nhật
+    publication_date: Optional[date] = None
     url: Optional[str] = None  # URL của publication có thể cập nhật
